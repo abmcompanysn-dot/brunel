19f5b886bb014a008fbcdb3db2951bf840cb056a
supervisioneed-code <supervisioneed@gmail.com>
2026-02-02 09:02:43 +0000

    Message clair sur les modifications

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 // URL de déploiement de votre script Google Apps Script en tant qu'API web.
 // IMPORTANT : Chaque fois que vous redéployez votre script, vous devez mettre à jour cette URL.
 
-const API_URL = "https://script.google.com/macros/s/AKfycbzu662uM-SA47RxTPvaHCuLl0NFWAcBNlWs_hIQckM0-AfjDw6dNFpQgW1h81f_XBdOWA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwwKpmyv_0nzVIq47ja2HAyWq1I56seBPIJh050Iy6weHOPZxcb2ulD0eqFqCpyq8Kiyg/exec";
 
 
